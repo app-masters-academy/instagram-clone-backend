ede248a3ec4e57bd0227f1fddf897aa290722004
Muky-dev <ti.gabrielfini@gmail.com>
2021-09-29 10:33:36 -0300

    improved code visualization

--- src/clients/clients.service.ts
+++ src/clients/clients.service.ts
@@ -1,49 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, CACHE_MANAGER, Inject } from '@nestjs/common';
+
+import { Cache } from 'cache-manager';
 
 import { CreateClientSheetDto } from './dto/create-client-sheet.dto';
 
 import { GithubService } from '../services/github.service';
 import { GoogleService } from 'src/services/googleSheet.service';
 
-import { createHash } from 'crypto';
+import { GoogleSpreadsheetRow } from 'google-spreadsheet';
 
 @Injectable()
 export class ClientsService {
   constructor(
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
     private readonly gitHubService: GithubService,
     private readonly googleSheet: GoogleService,
   ) {}
 
-  private async queryClient(email: string) {
-    const sheet = (await this.googleSheet.getDoc()).sheetsByIndex[0];
-
-    const rows = await sheet.getRows();
-    const user = rows.filter((row) => {
-      return row.email === email;
-    });
-    return user;
-  }
-
-  async sheetRegister(body: CreateClientSheetDto) {
-    const { email, github, name } = body;
+  async login(body: CreateClientSheetDto) {
+    const { email, github } = body;
     const parsedEmail = email.trim().toLowerCase();
+    const cachedUser: GoogleSpreadsheetRow = await this.cacheManager.get(
+      parsedEmail,
+    );
+    if (cachedUser) {
+      return { token: cachedUser.token };
+    }
     const githubExists = await this.gitHubService.verifyGithub(github);
     if (!githubExists) {
       throw new Error("Provided GitHub profile don't exists");
     }
-    const user = await this.queryClient(parsedEmail);
-    if (user.length == 0) {
-      const sheet = (await this.googleSheet.getDoc()).sheetsByIndex[0];
-      const token = createHash('md5').update(parsedEmail).digest('hex');
-      const row = await sheet.addRow({
-        email: parsedEmail,
-        github: github.trim().toLowerCase(),
-        name: name,
-        token: token,
-      });
-      return { token: row.token };
-    } else {
-      return { token: user[0].token };
+    const user: GoogleSpreadsheetRow = await this.googleSheet.querySheet(
+      parsedEmail,
+    );
+    if (user) {
+      return { token: user.token };
     }
+
+    const addedClient = await this.googleSheet.addToSheet(body);
+
+    return { token: addedClient.token };
   }
 }
--- src/services/googleSheet.service.ts
+++ src/services/googleSheet.service.ts
@@ -1,5 +1,9 @@
-import { Injectable } from '@nestjs/common';
-import { GoogleSpreadsheet } from 'google-spreadsheet';
+import { Injectable, CACHE_MANAGER, Inject } from '@nestjs/common';
+import { GoogleSpreadsheet, GoogleSpreadsheetRow } from 'google-spreadsheet';
+import { Cache } from 'cache-manager';
+import { createHash } from 'crypto';
+
+import { CreateClientSheetDto } from 'src/clients/dto/create-client-sheet.dto';
 
 @Injectable()
 export class GoogleService {
@@ -8,7 +12,7 @@ export class GoogleService {
   private privateKey: string;
   private doc: GoogleSpreadsheet;
 
-  constructor() {
+  constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {
     this.sheetId = process.env.SHEET_ID;
     this.sheetEmail = process.env.SHEET_API_EMAIL;
     this.privateKey = process.env.SHEET_API_PRIVATE_KEY;
@@ -22,5 +26,33 @@ export class GoogleService {
   async getDoc() {
     await this.doc.loadInfo();
     return this.doc;
+  }
+
+  async querySheet(email: string): Promise<GoogleSpreadsheetRow> {
+    const sheet = (await this.getDoc()).sheetsByIndex[0];
+    const parsedEmail = email.trim().toLowerCase();
+    const rows = await sheet.getRows();
+    rows.forEach(async (row) => {
+      await this.cacheManager.set(row.email, row);
+    });
+    const user = rows.filter((row) => {
+      return row.email === parsedEmail;
+    });
+    return user[0];
+  }
+
+  async addToSheet(user: CreateClientSheetDto): Promise<GoogleSpreadsheetRow> {
+    const { email, github, name } = user;
+    const parsedEmail = email.trim().toLowerCase();
+    const sheet = (await this.getDoc()).sheetsByIndex[0];
+    const token = createHash('md5').update(parsedEmail).digest('hex');
+    const row = await sheet.addRow({
+      email: parsedEmail,
+      github: github.trim().toLowerCase(),
+      name: name.toLowerCase(),
+      token: token,
+    });
+    await this.cacheManager.set(row.email, row);
+    return row;
   }
 }

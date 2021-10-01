fbb44f0fafae20bf73a183646422f49250dca7c4
Muky-dev <ti.gabrielfini@gmail.com>
2021-10-01 15:34:36 -0300

    removed import of deleted service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,8 @@
 import { Controller, Get, Res } from '@nestjs/common';
 import { Response } from 'express';
-import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
   getDoc(@Res() res: Response) {
     const docUrl = 'https://documenter.getpostman.com/view/17473013/UUy38mGg';

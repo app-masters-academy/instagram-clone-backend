dd056ed1e255f48b16ee1d73a0b80937413a8811
Muky-dev <ti.gabrielfini@gmail.com>
2021-10-04 15:51:09 -0300

    finally fixed db

--- src/config/db.ts
+++ src/config/db.ts
@@ -26,7 +26,7 @@ const config: ConnectionOptions = {
   // Allow both start:prod and start:dev to use migrations
   // __dirname is either dist or src folder, meaning either
   // the compiled js in prod or the ts in dev.
-  migrations: [__dirname + 'dist/migrations/**/*.js'],
+  migrations: [__dirname + '/../migrations/*{.ts,.js}'],
   migrationsTableName: 'react_for_humans',
   cli: {
     // Location of migration should be inside src folder

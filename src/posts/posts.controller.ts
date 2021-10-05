a56951ca1e415f8ccc08477f43a6174a4acefc51
Muky-dev <ti.gabrielfini@gmail.com>
2021-10-05 13:25:22 -0300

    Delete post endpoint added

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -5,8 +5,8 @@ import {
   Body,
   UseGuards,
   Req,
-  UseInterceptors,
-  UploadedFile,
+  Delete,
+  Param,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { RealIP } from 'nestjs-real-ip';
@@ -15,9 +15,6 @@ import { PostsService } from './posts.service';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UserDto } from 'src/users/dto/user.dto';
 import { ClientDto } from 'src/clients/dto/client.dto';
-import { FileInterceptor } from '@nestjs/platform-express';
-import { diskStorage } from 'multer';
-import path from 'path';
 
 @Controller('post')
 export class PostsController {
@@ -25,37 +22,26 @@ export class PostsController {
 
   @Post()
   @UseGuards(AuthGuard())
-  @UseInterceptors(
-    FileInterceptor('file', {
-      limits: { files: 1 },
-      fileFilter: (req, file, cb) => {
-        if (file.mimetype !== 'image/png') {
-          return cb(new Error('File must be an image'), false);
-        }
-        cb(null, true);
-      },
-      storage: diskStorage({
-        destination: 'public/uploads/postImage/',
-        filename: (req, file, cb) => {
-          cb(null, Date.now() + path.extname(file.originalname));
-        },
-      }),
-    }),
-  )
   create(
     @Body() createPostDto: CreatePostDto,
-    @UploadedFile() file: Express.Multer.File,
     @Req() req: any,
     @RealIP() ip: string,
   ) {
     const user = <UserDto>req.user;
     const client = <ClientDto>req.userClient;
-    return this.postsService.createPost(createPostDto, user, client, ip, file);
+    return this.postsService.createPost(createPostDto, user, client, ip);
   }
 
   @Get()
   findAll(@Req() req: any) {
     const client = <ClientDto>req.userClient;
     return this.postsService.findAll(client);
+  }
+
+  @UseGuards(AuthGuard())
+  @Delete('/:id')
+  deletePost(@Param('id') id: string, @Req() req: any) {
+    const user = <UserDto>req.user;
+    return this.postsService.deletePost(id, user);
   }
 }

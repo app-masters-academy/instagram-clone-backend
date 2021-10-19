97a51442fd8178436366abf7a6dc6cd247a95358
Muky-dev <ti.gabrielfini@gmail.com>
2021-10-19 13:25:46 -0300

    validate if photoUrl is an url

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,10 +1,12 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsUrl } from 'class-validator';
 import { UserDto } from 'src/users/dto/user.dto';
 
 export class CreatePostDto {
-  @IsNotEmpty({ message: 'Provide a description' })
+  @IsNotEmpty({ message: 'O post precisa de uma descrição' })
   description: string;
 
+  @IsOptional()
+  @IsUrl()
   photoUrl: string;
 
   userId: UserDto;

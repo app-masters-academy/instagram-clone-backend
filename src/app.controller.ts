336ec1df1af0bf82b48a251040333c588a5ea819
Muky-dev <ti.gabrielfini@gmail.com>
2021-10-19 14:46:07 -0300

    root route now display doc and repo

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,17 @@
-import { Controller, Get, Res } from '@nestjs/common';
+import { Controller, Get, Render, Res } from '@nestjs/common';
 import { Response } from 'express';
 
 @Controller()
 export class AppController {
+  @Get()
+  @Render('index')
+  root() {
+    return {
+      docUrl: 'https://documenter.getpostman.com/view/17473013/UUy38mGg',
+      apiRepo: 'https://github.com/app-masters-academy/instagram-clone-backend',
+    };
+  }
+
   @Get('/docs')
   getDoc(@Res() res: Response) {
     const docUrl = 'https://documenter.getpostman.com/view/17473013/UUy38mGg';
